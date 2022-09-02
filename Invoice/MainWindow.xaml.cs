486a6e9c28996c4b46accae7c1ad689b49987545
bojanmaric <bojanmaric98@gmail.com>
2022-09-02 13:33:30 +0200

    Update and Delete finished

--- Invoice/MainWindow.xaml.cs
+++ Invoice/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ using DocumentFormat.OpenXml.Spreadsheet;
 using DocumentFormat.OpenXml;
 using GemBox.Spreadsheet;
 using System.Text.RegularExpressions;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 
 namespace Invoice
@@ -42,7 +43,7 @@ namespace Invoice
         public string pdvLevel { get; set; }
 
         public double pdvValue { get; set; }
-       
+
 
         public double sumPrice { get; set; }
 
@@ -56,8 +57,8 @@ namespace Invoice
     public partial class MainWindow : Window
     {
         DataTable dt;
-        public List<Article> articles= new List<Article>();
-          
+        public List<Article> articles = new List<Article>();
+
         public Customer customer = new Customer();
 
         public string dateCreate { get; set; }
@@ -69,6 +70,11 @@ namespace Invoice
         double sumAllPDV = 0;
         double sumAllPoreskuOsnovicu = 0;
         double summAllAmountOfArticles = 0;
+
+        private bool selectedCell = false;
+        private int idSelectedRow;
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -103,7 +109,7 @@ namespace Invoice
 
             txtQuantity.Text = "1";
             txtDiscount.Text = "0";
-            
+
             txtNameArt.Focus();
 
         }
@@ -127,7 +133,7 @@ namespace Invoice
             }
             else
             {
-                MessageBox.Show("Morate popuniti sva polja!","Greška", MessageBoxButton.OK,MessageBoxImage.Error);
+                MessageBox.Show("Morate popuniti sva polja!", "Greška", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
 
@@ -135,35 +141,36 @@ namespace Invoice
         //function where we need to add new article in list and also add new row in data grid
         public void displayData()
         {
-           // double rabat = double.Parse(txtDiscount.Text);
-            double price=double.Parse(txtPriceArt.Text);
+            // double rabat = double.Parse(txtDiscount.Text);
+            double price = double.Parse(txtPriceArt.Text);
 
-            double poreskaOsnovica = Math.Round((price/((100+double.Parse(cmbPDV.SelectedValue.ToString()))/100))*double.Parse(txtQuantity.Text),2);
+            double poreskaOsnovica = Math.Round((price / ((100 + double.Parse(cmbPDV.SelectedValue.ToString())) / 100)) * double.Parse(txtQuantity.Text), 2);
 
-            double sumCell=Math.Round(price*double.Parse(txtQuantity.Text),2);
-            double pdvValue = Math.Round(sumCell -poreskaOsnovica,2);
+            double sumCell = Math.Round(price * double.Parse(txtQuantity.Text), 2);
+            double pdvValue = Math.Round(sumCell - poreskaOsnovica, 2);
             //Math.round(value,2) to round valute to two decimal place
 
-            articles.Add(new Article() {
-                idArt=txtIDArticle.Text,
-                nameArt=txtNameArt.Text,
-                measureUnit= cmbMeasureUnit.SelectedValue.ToString() !="" ?  cmbMeasureUnit.SelectedValue.ToString():"kom",
-                quatity=double.Parse(txtQuantity.Text),
-                price= price,
-                poreskaOsnovica=poreskaOsnovica,
-                pdvLevel=cmbPDV.SelectedValue.ToString()!="" ? cmbPDV.SelectedValue.ToString():"20",
-                pdvValue=pdvValue,
-                sumPrice=sumCell,
+            articles.Add(new Article()
+            {
+                idArt = txtIDArticle.Text,
+                nameArt = txtNameArt.Text,
+                measureUnit = cmbMeasureUnit.SelectedValue.ToString() != "" ? cmbMeasureUnit.SelectedValue.ToString() : "kom",
+                quatity = double.Parse(txtQuantity.Text),
+                price = price,
+                poreskaOsnovica = poreskaOsnovica,
+                pdvLevel = cmbPDV.SelectedValue.ToString() != "" ? cmbPDV.SelectedValue.ToString() : "20",
+                pdvValue = pdvValue,
+                sumPrice = sumCell,
 
             });
 
-            sumAllPDV =Math.Round( sumAllPDV + pdvValue,2);
+            sumAllPDV = Math.Round(sumAllPDV + pdvValue, 2);
             txtPdvSum.Text = sumAllPDV.ToString();
 
-            summAllAmountOfArticles = Math.Round(summAllAmountOfArticles + sumCell,2);
+            summAllAmountOfArticles = Math.Round(summAllAmountOfArticles + sumCell, 2);
             txtAmountOfAllAcrticles.Text = summAllAmountOfArticles.ToString();
 
-            sumAllPoreskuOsnovicu= Math.Round(sumAllPoreskuOsnovicu + poreskaOsnovica,2);
+            sumAllPoreskuOsnovicu = Math.Round(sumAllPoreskuOsnovicu + poreskaOsnovica, 2);
             txtNetoSum.Text = sumAllPoreskuOsnovicu.ToString();
 
             DataRow dr = dt.NewRow();
@@ -180,20 +187,20 @@ namespace Invoice
 
             dt.Rows.Add(dr);
 
+            emptyRow();
         }
 
         //creating the name of file for invoice
         public string getPathNameForNewFile()
         {
-            SaveFileDialog path=new SaveFileDialog();
-            
+            SaveFileDialog path = new SaveFileDialog();
+
             if (path.ShowDialog() == true)
             {
                 return path.FileName;
             }
             return "";
         }
-
 
         //Funcition where we create Invoice and fill header cells in template.xlsx
         //and copy data from dataGrid to place which is planned to be fill with articles
@@ -246,7 +253,7 @@ namespace Invoice
                     worksheet.Cells[$"D{21 + i}"].Value = articles[i].measureUnit;
                     //Quantitiy of articles
                     worksheet.Cells[$"E{21 + i}"].Value = articles[i].quatity;
-                    worksheet.Cells[$"E{21 + i}"].Style.HorizontalAlignment=HorizontalAlignmentStyle.Center; 
+                    worksheet.Cells[$"E{21 + i}"].Style.HorizontalAlignment = HorizontalAlignmentStyle.Center;
 
                     //price of article per one
                     worksheet.Cells[$"F{21 + i}"].Value = articles[i].price;
@@ -257,7 +264,7 @@ namespace Invoice
                     worksheet.Cells[$"G{21 + i}"].Style.NumberFormat = "#,##0.00";
 
                     //rate PDV 
-                    worksheet.Cells[$"H{21 + i}"].Value = articles[i].pdvLevel+"%";
+                    worksheet.Cells[$"H{21 + i}"].Value = articles[i].pdvLevel + "%";
                     worksheet.Cells[$"H{21 + i}"].Style.HorizontalAlignment = HorizontalAlignmentStyle.Center;
 
                     //sum of pdv
@@ -272,11 +279,11 @@ namespace Invoice
                     if (i == count - 1)
                     {
 
-                        worksheet.Cells[$"J{21+count }"].Formula = $"=Sum({worksheet.Cells["G21"]}:{worksheet.Cells["G"+(21+i)]})";
-                        worksheet.Cells[$"J{21+count + 1}"].Formula = $"=Sum({worksheet.Cells["I21"]}:{worksheet.Cells["I"+(21 + i)]})";
-                        worksheet.Cells[$"J{21+count + 2}"].Formula = $"=Sum({worksheet.Cells["J21"]}:{worksheet.Cells["J"+ (21 + i)]})";
-                       // worksheet.Cells[$"J{21 + count + 3}"].Value = double.Parse(txtAvans.Text);
-                        worksheet.Cells[$"J{21 + count + 4}"].Formula = $"=Sum({worksheet.Cells["J"+(21 + count + 2)]}-{worksheet.Cells["J" + (21 + count + 3)]})";
+                        worksheet.Cells[$"J{21 + count}"].Formula = $"=Sum({worksheet.Cells["G21"]}:{worksheet.Cells["G" + (21 + i)]})";
+                        worksheet.Cells[$"J{21 + count + 1}"].Formula = $"=Sum({worksheet.Cells["I21"]}:{worksheet.Cells["I" + (21 + i)]})";
+                        worksheet.Cells[$"J{21 + count + 2}"].Formula = $"=Sum({worksheet.Cells["J21"]}:{worksheet.Cells["J" + (21 + i)]})";
+                        // worksheet.Cells[$"J{21 + count + 3}"].Value = double.Parse(txtAvans.Text);
+                        worksheet.Cells[$"J{21 + count + 4}"].Formula = $"=Sum({worksheet.Cells["J" + (21 + count + 2)]}-{worksheet.Cells["J" + (21 + count + 3)]})";
                         worksheet.Cells[$"D{21 + count + 5}"].Formula = txtAmmountMoneySpell.Text;
 
 
@@ -284,7 +291,7 @@ namespace Invoice
                     }
 
                 }
-            
+
                 string path = getPathNameForNewFile();
 
                 if (path != "")
@@ -300,31 +307,163 @@ namespace Invoice
             }
             else
             {
-                MessageBox.Show("Niste uneli sva polja", "Greška",MessageBoxButton.OK,MessageBoxImage.Error);
+                MessageBox.Show("Niste uneli sva polja", "Greška", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
         }
 
+        // Empty all textboxes which are filled with data
+        public void emptyRow()
+        {
+            txtIDArticle.Text = "";
+            txtNameArt.Text = "";
+            txtQuantity.Text = "1";
+            txtPriceArt.Text = "";
+        }
 
-        //control text box is it input only decimal numbers
+        // Secting the one cell for editing or deleting
+        // If is table emtry selected index is -1
+        // we disable button from add new row and enable others
+        private void dataGridInvoice_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var selectedRow = dataGridInvoice.SelectedIndex;
+           
+            if (selectedRow >= 0)
+            {
+                this.selectedCell = true;
+                this.idSelectedRow = selectedRow;
+
+                Article selectedArticle = articles[selectedRow];
+
+                txtIDArticle.Text = selectedArticle.idArt;
+                txtNameArt.Text = selectedArticle.nameArt;
+                txtQuantity.Text = selectedArticle.quatity.ToString();
+                txtPriceArt.Text = selectedArticle.price.ToString();
+                cmbMeasureUnit.Text = selectedArticle.measureUnit;
+                cmbPDV.Text = selectedArticle.pdvLevel;
+
+                btnUpdateCell.IsEnabled = true;
+                btnDeleteCell.IsEnabled = true;
+                btnCancel.IsEnabled = true;
+                btnAddCell.IsEnabled = false;
+
+            }
+        }
+        // Update selected row in data grid
+        private void btnUpdateCell_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Da li ste sigurni?", "Brisanje", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+
+                // Fist of all we need to set the values like befoure becouse in next steps we have to set updated values
+                double price = articles[idSelectedRow].price;
+                double poreskaOsnovica = Math.Round((price / ((100 + double.Parse(cmbPDV.SelectedValue.ToString())) / 100)) * articles[idSelectedRow].quatity, 2);
+                double sumCell = Math.Round(price * articles[idSelectedRow].quatity, 2);
+                double pdvValue = Math.Round(sumCell - poreskaOsnovica, 2);
+                
+                sumAllPDV = Math.Round(sumAllPDV - pdvValue, 2);
+                summAllAmountOfArticles = Math.Round(summAllAmountOfArticles - sumCell, 2);
+                sumAllPoreskuOsnovicu = Math.Round(sumAllPoreskuOsnovicu - poreskaOsnovica, 2);
+
+                // Set the new values in variables
+                price = double.Parse(txtPriceArt.Text);
+                poreskaOsnovica = Math.Round((price / ((100 + double.Parse(cmbPDV.SelectedValue.ToString())) / 100)) * double.Parse(txtQuantity.Text), 2);
+                sumCell = Math.Round(price * double.Parse(txtQuantity.Text), 2);
+                pdvValue = Math.Round(sumCell - poreskaOsnovica, 2);
+
+                articles[idSelectedRow].idArt = txtIDArticle.Text;
+                articles[idSelectedRow].nameArt = txtNameArt.Text;
+                articles[idSelectedRow].measureUnit = cmbMeasureUnit.SelectedValue.ToString() != "" ? cmbMeasureUnit.SelectedValue.ToString() : "kom";
+                articles[idSelectedRow].quatity = double.Parse(txtQuantity.Text);
+                articles[idSelectedRow].price = price;
+                articles[idSelectedRow].poreskaOsnovica = poreskaOsnovica;
+                articles[idSelectedRow].pdvLevel = cmbPDV.SelectedValue.ToString() != "" ? cmbPDV.SelectedValue.ToString() : "20";
+                articles[idSelectedRow].pdvValue = pdvValue;
+                articles[idSelectedRow].sumPrice = sumCell;
+
+                sumAllPDV = Math.Round(sumAllPDV + pdvValue, 2);
+                txtPdvSum.Text = sumAllPDV.ToString();
+
+                summAllAmountOfArticles = Math.Round(summAllAmountOfArticles + sumCell, 2);
+                txtAmountOfAllAcrticles.Text = summAllAmountOfArticles.ToString();
+
+                sumAllPoreskuOsnovicu = Math.Round(sumAllPoreskuOsnovicu + poreskaOsnovica, 2);
+                txtNetoSum.Text = sumAllPoreskuOsnovicu.ToString();
+
+                dt.Rows[idSelectedRow][0] = txtIDArticle.Text;
+                dt.Rows[idSelectedRow][1] = txtNameArt.Text;
+                dt.Rows[idSelectedRow][2] = cmbMeasureUnit.SelectedValue.ToString();
+                dt.Rows[idSelectedRow][3] = txtQuantity.Text;
+                dt.Rows[idSelectedRow][4] = txtPriceArt.Text;
+                dt.Rows[idSelectedRow][5] = poreskaOsnovica.ToString();//poreska osnovica
+                dt.Rows[idSelectedRow][6] = cmbPDV.SelectedValue.ToString();
+                dt.Rows[idSelectedRow][7] = pdvValue.ToString();
+                dt.Rows[idSelectedRow][8] = sumCell.ToString();
+
+                emptyRow();
+                ResetBtn();
+
+            }
+
+        }
+        // Delete selected row in data grid
+        private void btnDeleteCell_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Da li ste sigurni?", "Brisanje", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                sumAllPDV = Math.Round(sumAllPDV - articles[idSelectedRow].pdvValue, 2);
+                txtPdvSum.Text = sumAllPDV.ToString();
+
+                summAllAmountOfArticles = Math.Round(summAllAmountOfArticles - articles[idSelectedRow].sumPrice, 2);
+                txtAmountOfAllAcrticles.Text = summAllAmountOfArticles.ToString();
+
+                sumAllPoreskuOsnovicu = Math.Round(sumAllPoreskuOsnovicu - articles[idSelectedRow].poreskaOsnovica, 2);
+                txtNetoSum.Text = sumAllPoreskuOsnovicu.ToString();
+
+                dt.Rows.RemoveAt(idSelectedRow);
+                articles.RemoveAt(idSelectedRow);
+                ResetBtn();
+                emptyRow();
+            }
+
+        }
+        public void ResetBtn()
+        {
+            btnUpdateCell.IsEnabled = false;
+            btnDeleteCell.IsEnabled = false;
+            btnCancel.IsEnabled = false;
+            btnAddCell.IsEnabled = true;
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ResetBtn();
+            emptyRow();
+        }
+
+        // Control text box is it input only decimal numbers
         private void txtPriceArt_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("^[.][0-9]+$|^[0-9]*[.]{0,1}[0-9]*$");
             e.Handled = !regex.IsMatch((sender as TextBox).Text.Insert((sender as TextBox).SelectionStart, e.Text));
         }
-        //control text box is it input only decimal numbers
+        // Control text box is it input only decimal numbers
         private void txtQuantity_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("^[.][0-9]+$|^[0-9]*[.]{0,1}[0-9]*$");
             e.Handled = !regex.IsMatch((sender as TextBox).Text.Insert((sender as TextBox).SelectionStart, e.Text));
         }
-        //control text box is it input only decimal numbers
-
+        // Control text box is it input only decimal numbers
         private void txtAvans_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
 
             Regex regex = new Regex("^[.][0-9]+$|^[0-9]*[.]{0,1}[0-9]*$");
             e.Handled = !regex.IsMatch((sender as TextBox).Text.Insert((sender as TextBox).SelectionStart, e.Text));
         }
+
+
     }
 }
